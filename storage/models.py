5e5e268c85f2ab0fdbce93ec4d23d840217a4e77
Takashi Ando <dodo5522@gmail.com>
2020-10-04 00:22:52 +0900

    Make tables by 'python manage.py makemigrations storage'

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,70 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Recorder(models.Model):
+    def record(self):
+        self.recorded_at = timezone.now()
+        self.save()
+
+
+class Location(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    zip_code = models.CharField(
+        verbose_name='郵便番号', max_length=8, blank=True,
+    )
+    country = models.CharField(
+        verbose_name='国', max_length=40, blank=True, default='日本',
+    )
+    prefecture = models.CharField(
+        verbose_name='都道府県', max_length=40, blank=True,
+    )
+    city = models.CharField(
+        verbose_name='市区町村番地', max_length=40, blank=True,
+    )
+    building = models.CharField(
+        verbose_name='建物名', max_length=40, blank=True,
+    )
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Unit(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    unit_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Group(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    group_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+class KilowattHours(Recorder):
+    created_at = models.DateTimeField(default=timezone.now)
+    recorded_at = models.DateTimeField(default=timezone.now)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=10, decimal_places=1)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class AmpHours(Recorder):
+    created_at = models.DateTimeField(default=timezone.now)
+    recorded_at = models.DateTimeField(default=timezone.now)
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    unit_id = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    value = models.DecimalField(max_digits=10, decimal_places=1)
+
+    def __str__(self):
+        return self.__class__.__name__
